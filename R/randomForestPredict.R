96d492a27703bdd0f3c862845ae2d682e9a2bdbd
MNLR <mikel.legasa@unican.es>
2020-11-03 15:34:22 +0100

    - Updated randomForestPredict()

--- R/randomForestPredict.R
+++ R/randomForestPredict.R
@@ -1,3 +1,11 @@
-randomForestPredict <- function(model, newdata){
-  colMeans(do.call(rbind, lapply(model, FUN = function(md) predict(md, newdata = newdata))))
+randomForestPredict <- function(model, newdata, bagging.function = mean){
+
+  prediction.type <- "matrix"
+
+  return(
+  apply(X = simplify2array(lapply(model,
+                                  FUN = function(md) predict(md, newdata = data.frame(newdata), type = prediction.type))
+                          ),
+        MARGIN = c(1,2), FUN = bagging.function)
+  )
 }
